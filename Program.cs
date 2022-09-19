89fb77ebc3fda2bc3a371e5366af9d81b0a5c679
Evgenii <evg4755@yandex.rugit config --global user.name evgeniieglobal user.email evg4755@yandex.ru>
2022-09-19 17:17:41 +0300

    Задача8

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int startNum = 1;
+while(startNum <= num)
+    {
+        if(startNum % 2 ==0)
+        Console.Write(startNum + ", ");
+        startNum ++;
+    }
